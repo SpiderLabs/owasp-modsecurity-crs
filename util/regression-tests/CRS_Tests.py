6b55d51f5c891ee723cd38aea5fd55fd2ba59759
Zack Allen <zma4580@gmail.com>
2017-07-18 20:52:28 -0400

    Fix spelling

--- util/regression-tests/CRS_Tests.py
+++ util/regression-tests/CRS_Tests.py
@@ -33,7 +33,7 @@ class FooLogChecker(logchecker.LogChecker):
         import datetime
         config = ConfigParser.ConfigParser()
         settings_file = os.path.abspath(os.path.dirname(os.path.dirname(__file__))) + '/settings.ini'
-        print('%s settings file location' % settings-file)
+        print('%s settings file location' % settings_file)
         config.read(settings_file)
         log_location = config.get('settings', 'log_location')
         our_logs = []
