8ae9c4b0e478f1767556871883f1ea826ca9c83b
Manuel Spartan <spartantri@users.noreply.github.com>
2018-01-30 11:19:27 +0100

    removed conflict

--- util/regression-tests/config.py
+++ util/regression-tests/config.py
@@ -1,12 +1,12 @@
 # Location of Apache Error Log
-log_location_linux = '/var/log/apache2/error.log'
+log_location_linux = '/var/log/httpd/error_log'
 log_location_windows = 'C:\Apache24\logs\error.log'
 
 # Regular expression to filter for timestamp in Apache Error Log
 #
 # Default timestamp format: (example: [Thu Nov 09 09:04:38.912314 2017])
 log_date_regex = "\[([A-Z][a-z]{2} [A-z][a-z]{2} \d{1,2} \d{1,2}\:\d{1,2}\:\d{1,2}\.\d+? \d{4})\]"
-#[Tue Jan 09 02:21:56.447586 2018]
+#
 # Reverse format: (example: [2017-11-09 08:25:03.002312])
 #log_date_regex = "\[([0-9-]{10} [0-9:.]{15})\]"
 
