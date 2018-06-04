4dc4a89df3a5e54bdbcf67c1f4a8d8c6970e603d
Chaim Sanders <chaim.sanders@gmail.com>
2018-06-04 11:05:12 -0400

    inital Docker Nginx support

--- util/regression-tests/CRS_Tests.py
+++ util/regression-tests/CRS_Tests.py
@@ -28,7 +28,7 @@ class FooLogChecker(logchecker.LogChecker):
                 else:
                     line += next_char
                 position -= 1
-            yield line[::-1]    
+            yield line[::-1]
 
     def get_logs(self):
         log_location = config.log_location_linux
@@ -38,18 +38,22 @@ class FooLogChecker(logchecker.LogChecker):
         our_logs = []
         for lline in self.reverse_readline(log_location):
             # Extract dates from each line
-            match = re.match(pattern,lline)
+            match = re.match(pattern, lline)
             if match:
                 log_date = match.group(1)
                 # Convert our date
                 log_date = datetime.datetime.strptime(log_date, log_date_format)
-                ftw_start = self.start
+                # NGINX doesn't give us microsecond level detail, truncate.
+                if log_location == "/usr/local/nginx/logs/error.log":
+                    ftw_start = self.start.replace(microsecond=0)
+                else:
+                    ftw_start = self.start
                 ftw_end = self.end
                 # If we have a log date in range
                 if log_date <= ftw_end and log_date >= ftw_start:
                     our_logs.append(lline)
                 # If our log is from before FTW started stop
-                if(log_date < ftw_start):
+                if log_date < ftw_start:
                     break
         return our_logs
 @pytest.fixture
