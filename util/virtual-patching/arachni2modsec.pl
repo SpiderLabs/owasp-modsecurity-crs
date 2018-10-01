0fe365c0407554679c9040134cb87481f1d4e2ce
Walter Hop <walter@lifeforms.nl>
2018-10-01 21:05:12 +0200

    Fix some additional variable cases in util files

--- util/virtual-patching/arachni2modsec.pl
+++ util/virtual-patching/arachni2modsec.pl
@@ -1,11 +1,11 @@
 #!/opt/local/bin/perl -T
 
 #############################################
-# -=[ Virtual Patching Converter Script ]=- # 
+# -=[ Virtual Patching Converter Script ]=- #
 #       Converts arachni XML Ouput          #
-#    https://github.com/Zapotek/arachni     # 
+#    https://github.com/Zapotek/arachni     #
 #                                           #
-#           arachni2modsec.pl               # 
+#           arachni2modsec.pl               #
 #             Version: 1.0                  #
 #                                           #
 #            Copyright 2011                 #
@@ -26,7 +26,7 @@ use Acme::Comment type=>'C++', one_line=>1; #Block commenting, can be removed la
 
 #############
 # Variables #
-############# 
+#############
 
 # [Configuration Vars]
 my %param;
@@ -51,7 +51,7 @@ my $VULN_CLASS_LFI = "Path Traversal";
 my $VULN_CLASS_RFI = "Remote file inclusion";
 my $VULN_CLASS_HTTPRS = "Response splitting";
 
-# Only the vulnerabilities in this array will have 
+# Only the vulnerabilities in this array will have
 # rules generated for them.
 my @supported_vulns = ($VULN_CLASS_XSS, $VULN_CLASS_SQLI, $VULN_CLASS_BLIND_SQLI, $VULN_CLASS_LFI, $VULN_CLASS_RFI, $VULN_CLASS_HTTPRS);
 
@@ -74,7 +74,7 @@ my $num_attacks_noflag=0;
 
 #############
 #   Main    #
-############# 
+#############
 
 # Clean up env so perl doesn't complain
 # when trying to run the restart snort
@@ -95,7 +95,7 @@ $vuln_count = 0;
 foreach my $current_type (@type){
 	print "==================================================================================================\n";
 	print "Vulnerability[$vuln_count] -  Type: $current_type\n";
-	
+
 	if(exists {map { $_ => 1 } @supported_vulns}->{$current_type}){
 		parseData(to_string($current_type));
 	}else {
@@ -137,13 +137,13 @@ sub parseData
 	my $id = $vuln_count;
 
 	print "Found a $vuln_str vulnerability.\n";
-	
+
 	$current_vuln_xml = XML::Smart->new($all_vulnerabilities_filename);
 	$current_vuln_url = $url[$vuln_count];
 
 	print URL_LIST "$current_vuln_url\n";
 
-	# Validate url (need seperate sub?) 
+	# Validate url (need seperate sub?)
 	print "Validating URL: $current_vuln_url\n";
 	if(is_uri(to_string($current_vuln_url))){
 		print "URL is well-formed\n";
@@ -151,32 +151,32 @@ sub parseData
 	} else {
 		print "URL is NOT well-formed. Breaking Out of Rule Generation\n";
 		$num_bad_urls++;
-		
+
 		# Waits for keypress in test mode so you can
 		# see why the URL failed validation.
 		if($test_mode){
 			wait_for_keypress();
-		}	
+		}
 		return;
 	}
-	
+
 	$current_uricontent = get_uricontent($current_vuln_url);
-	
-	
+
+
 	# Only need param if XSS attack,SQLINJ,XPATH
 	# and maybe for HTTPRS, DT.
 	# NOT for PRL and DI
-	
+
 	if(($vuln_str ne $VULN_CLASS_PRL) && ($vuln_str ne $VULN_CLASS_DI)){
 		@current_params = $param[$vuln_count];
-		
+
 	}
-	if(($vuln_str ne $VULN_CLASS_PRL) && ($vuln_str ne $VULN_CLASS_DI)){	
+	if(($vuln_str ne $VULN_CLASS_PRL) && ($vuln_str ne $VULN_CLASS_DI)){
 			print "Current vulnerable Param(s): @current_params\n";
 	}
-	
-	generate_patch($vuln_str,$current_uricontent,@current_params);	
-	
+
+	generate_patch($vuln_str,$current_uricontent,@current_params);
+
 
 }
 
@@ -189,37 +189,37 @@ sub generate_patch
 
 	switch($type)
 	{
-		case ($VULN_CLASS_XSS) 
-		{	
+		case ($VULN_CLASS_XSS)
+		{
 			if($uricontent ne "" && @params){
 				foreach(@params){
 					if($_ ne ""){
 						# Check to see if each vulnerable parameter is valid
 						# then generate a rule using both uricontent and the
 						# parameter
-						$rule = "SecRule REQUEST_FILENAME \"$uricontent\" \"chain,phase:2,t:none,block,msg:'Virtual Patch for $type',id:'$id',tag:'WEB_ATTACK/XSS',tag:'WASCTC/WASC-8',tag:'WASCTC/WASC-22',tag:'OWASP_TOP_10/A2',tag:'OWASP_AppSensor/IE1',tag:'PCI/6.5.1',logdata:'%{MATCHED_VAR_name}',severity:'2'\"\n\tSecRule \&TX:\'\/XSS.*ARGS:$_\/\' \"\@gt 0\" \"setvar:'tx.msg=%{rule.msg}',setvar:tx.xss_score=+%{tx.critical_anomaly_score},setvar:tx.anomaly_score=+%{tx.critical_anomaly_score}\"";
-						
+						$rule = "SecRule REQUEST_FILENAME \"$uricontent\" \"chain,phase:2,t:none,block,msg:'Virtual Patch for $type',id:'$id',tag:'WEB_ATTACK/XSS',tag:'WASCTC/WASC-8',tag:'WASCTC/WASC-22',tag:'OWASP_TOP_10/A2',tag:'OWASP_AppSensor/IE1',tag:'PCI/6.5.1',logdata:'%{MATCHED_VAR_NAME}',severity:'2'\"\n\tSecRule \&TX:\'\/XSS.*ARGS:$_\/\' \"\@gt 0\" \"setvar:'tx.msg=%{rule.msg}',setvar:tx.xss_score=+%{tx.critical_anomaly_score},setvar:tx.anomaly_score=+%{tx.critical_anomaly_score}\"";
+
 							print $MODSEC_RULES "#\n# Arachni Virtual Patch Details:\n# ID: $id\n# Type: $type\n# Vulnerable URL: $uricontent\n# Vulnerable Parameter: $_\n#\n".$rule."\n\n";
 							print "$VULN_CLASS_XSS (uricontent and param) rule successfully generated and saved in $modsec_rules_file.\n";
 							$num_rules_generated++;
 					}
 				}
-			} 
+			}
 		}
-		
+
 		case ($VULN_CLASS_SQLI)
 		{
-		
+
 			if($uricontent ne "" && @params){
 				foreach(@params){
 					if($_ ne ""){
-						$rule = "SecRule REQUEST_FILENAME \"$uricontent\" \"chain,phase:2,t:none,block,msg:'Virtual Patch for $type',id:'$id',tag:'WEB_ATTACK/SQL_INJECTION',tag:'WASCTC/WASC-19',tag:'OWASP_TOP_10/A1',tag:'OWASP_AppSensor/CIE1',tag:'PCI/6.5.2',logdata:'%{MATCHED_VAR_name}',severity:'2'\"\n\tSecRule \&TX:\'\/SQL_INJECTION.*ARGS:$_\/\' \"\@gt 0\" \"setvar:'tx.msg=%{rule.msg}',setvar:tx.sql_injection_score=+%{tx.critical_anomaly_score},setvar:tx.anomaly_score=+%{tx.critical_anomaly_score}\"";
-				
+						$rule = "SecRule REQUEST_FILENAME \"$uricontent\" \"chain,phase:2,t:none,block,msg:'Virtual Patch for $type',id:'$id',tag:'WEB_ATTACK/SQL_INJECTION',tag:'WASCTC/WASC-19',tag:'OWASP_TOP_10/A1',tag:'OWASP_AppSensor/CIE1',tag:'PCI/6.5.2',logdata:'%{MATCHED_VAR_NAME}',severity:'2'\"\n\tSecRule \&TX:\'\/SQL_INJECTION.*ARGS:$_\/\' \"\@gt 0\" \"setvar:'tx.msg=%{rule.msg}',setvar:tx.sql_injection_score=+%{tx.critical_anomaly_score},setvar:tx.anomaly_score=+%{tx.critical_anomaly_score}\"";
+
 					print $MODSEC_RULES "#\n# Arachni Virtual Patch Details:\n# ID: $id\n# Type: $type\n# Vulnerable URL: $uricontent\n# Vulnerable Parameter: $_\n#\n".$rule."\n\n";
                                         print "$VULN_CLASS_SQLI (uricontent and param) rule successfully generated and saved in $modsec_rules_file.\n";
                                         $num_rules_generated++;
-		
-										
+
+
 					}
 				}
 			}
@@ -231,7 +231,7 @@ sub generate_patch
                         if($uricontent ne "" && @params){
                                 foreach(@params){
                                         if($_ ne ""){
-                                                $rule = "SecRule REQUEST_FILENAME \"$uricontent\" \"chain,phase:2,t:none,block,msg:'Virtual Patch for $type',id:'$id',tag:'WEB_ATTACK/SQL_INJECTION',tag:'WASCTC/WASC-19',tag:'OWASP_TOP_10/A1',tag:'OWASP_AppSensor/CIE1',tag:'PCI/6.5.2',logdata:'%{MATCHED_VAR_name}',severity:'2'\"\n\tSecRule \&TX:\'\/SQL_INJECTION.*ARGS:$_\/\' \"\@gt 0\" \"setvar:'tx.msg=%{rule.msg}',setvar:tx.sql_injection_score=+%{tx.critical_anomaly_score},setvar:tx.anomaly_score=+%{tx.critical_anomaly_score}\"";
+                                                $rule = "SecRule REQUEST_FILENAME \"$uricontent\" \"chain,phase:2,t:none,block,msg:'Virtual Patch for $type',id:'$id',tag:'WEB_ATTACK/SQL_INJECTION',tag:'WASCTC/WASC-19',tag:'OWASP_TOP_10/A1',tag:'OWASP_AppSensor/CIE1',tag:'PCI/6.5.2',logdata:'%{MATCHED_VAR_NAME}',severity:'2'\"\n\tSecRule \&TX:\'\/SQL_INJECTION.*ARGS:$_\/\' \"\@gt 0\" \"setvar:'tx.msg=%{rule.msg}',setvar:tx.sql_injection_score=+%{tx.critical_anomaly_score},setvar:tx.anomaly_score=+%{tx.critical_anomaly_score}\"";
 
                                         print $MODSEC_RULES "#\n# Arachni Virtual Patch Details:\n# ID: $id\n# Type: $type\n# Vulnerable URL: $uricontent\n# Vulnerable Parameter: $_\n#\n".$rule."\n\n";
                                         print "$VULN_CLASS_SQLI (uricontent and param) rule successfully generated and saved in $modsec_rules_file.\n";
@@ -248,7 +248,7 @@ sub generate_patch
 			if($uricontent ne "" && @params){
 				foreach(@params){
 					if($_ ne ""){
-                                                $rule = "SecRule REQUEST_FILENAME \"$uricontent\" \"chain,phase:2,t:none,block,msg:'Virtual Patch for $type',id:'$id',tag:'WEB_ATTACK/LFI',tag:'WASCTC/WASC-33',logdata:'%{MATCHED_VAR_name}',severity:'2'\"\n\tSecRule \&TX:\'\/LFI.*ARGS:$_\/\' \"\@gt 0\" \"setvar:'tx.msg=%{rule.msg}',setvar:tx.anomaly_score=+%{tx.critical_anomaly_score}\"";
+                                                $rule = "SecRule REQUEST_FILENAME \"$uricontent\" \"chain,phase:2,t:none,block,msg:'Virtual Patch for $type',id:'$id',tag:'WEB_ATTACK/LFI',tag:'WASCTC/WASC-33',logdata:'%{MATCHED_VAR_NAME}',severity:'2'\"\n\tSecRule \&TX:\'\/LFI.*ARGS:$_\/\' \"\@gt 0\" \"setvar:'tx.msg=%{rule.msg}',setvar:tx.anomaly_score=+%{tx.critical_anomaly_score}\"";
 
                                         print $MODSEC_RULES "#\n# Arachni Virtual Patch Details:\n# ID: $id\n# Type: $type\n# Vulnerable URL: $uricontent\n# Vulnerable Parameter: $_\n#\n".$rule."\n\n";
                                         print "$VULN_CLASS_LFI (uricontent and param) rule successfully generated and saved in $modsec_rules_file.\n";
@@ -265,7 +265,7 @@ sub generate_patch
                         if($uricontent ne "" && @params){
                                 foreach(@params){
                                         if($_ ne ""){
-                                                $rule = "SecRule REQUEST_FILENAME \"$uricontent\" \"chain,phase:2,t:none,block,msg:'Virtual Patch for $type',id:'$id',tag:'WEB_ATTACK/RFI',tag:'WASCTC/WASC-05',logdata:'%{MATCHED_VAR_name}',severity:'2'\"\n\tSecRule \&TX:\'\/RFI.*ARGS:$_\/\' \"\@gt 0\" \"setvar:'tx.msg=%{rule.msg}',setvar:tx.anomaly_score=+%{tx.critical_anomaly_score}\"";
+                                                $rule = "SecRule REQUEST_FILENAME \"$uricontent\" \"chain,phase:2,t:none,block,msg:'Virtual Patch for $type',id:'$id',tag:'WEB_ATTACK/RFI',tag:'WASCTC/WASC-05',logdata:'%{MATCHED_VAR_NAME}',severity:'2'\"\n\tSecRule \&TX:\'\/RFI.*ARGS:$_\/\' \"\@gt 0\" \"setvar:'tx.msg=%{rule.msg}',setvar:tx.anomaly_score=+%{tx.critical_anomaly_score}\"";
 
                                         print $MODSEC_RULES "#\n# Arachni Virtual Patch Details:\n# ID: $id\n# Type: $type\n# Vulnerable URL: $uricontent\n# Vulnerable Parameter: $_\n#\n".$rule."\n\n";
                                         print "$VULN_CLASS_LFI (uricontent and param) rule successfully generated and saved in $modsec_rules_file.\n";
@@ -282,7 +282,7 @@ sub generate_patch
                         if($uricontent ne "" && @params){
                                 foreach(@params){
                                         if($_ ne ""){
-                                                $rule = "SecRule REQUEST_FILENAME \"$uricontent\" \"chain,phase:2,t:none,block,msg:'Virtual Patch for $type',id:'$id',tag:'WEB_ATTACK/RESPONSE_SPLITTING',tag:'WASCTC/WASC-25',logdata:'%{MATCHED_VAR_name}',severity:'2'\"\n\tSecRule \&TX:\'\/RESPONSE_SPLITTING.*ARGS:$_\/\' \"\@gt 0\" \"setvar:'tx.msg=%{rule.msg}',setvar:tx.anomaly_score=+%{tx.critical_anomaly_score}\"";
+                                                $rule = "SecRule REQUEST_FILENAME \"$uricontent\" \"chain,phase:2,t:none,block,msg:'Virtual Patch for $type',id:'$id',tag:'WEB_ATTACK/RESPONSE_SPLITTING',tag:'WASCTC/WASC-25',logdata:'%{MATCHED_VAR_NAME}',severity:'2'\"\n\tSecRule \&TX:\'\/RESPONSE_SPLITTING.*ARGS:$_\/\' \"\@gt 0\" \"setvar:'tx.msg=%{rule.msg}',setvar:tx.anomaly_score=+%{tx.critical_anomaly_score}\"";
 
                                         print $MODSEC_RULES "#\n# Arachni Virtual Patch Details:\n# ID: $id\n# Type: $type\n# Vulnerable URL: $uricontent\n# Vulnerable Parameter: $_\n#\n".$rule."\n\n";
                                         print "$VULN_CLASS_RFI (uricontent and param) rule successfully generated and saved in $modsec_rules_file.\n";
@@ -306,8 +306,8 @@ sub get_uricontent
 	# http://.../
 	$url =~ /$regex/;
 	substr($url,index($url,$&),length($&)) = "";
-	
-	# If the URL contains a php or cgi query with 
+
+	# If the URL contains a php or cgi query with
 	# one or more params and values, trim those out.
 	# Trim from the question mark to the end.
 	if($url =~ /\?/){
