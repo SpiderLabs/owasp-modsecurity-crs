c48e66574f5c0ae056f9adfb44709cbd0c157ef2
csanders-git <chaim@chaimsanders.com>
2019-05-29 15:30:18 -0700

    proxy.conf doesn't exist yet so we don't need any of its logic

--- util/docker-entrypoint.sh
+++ util/docker-entrypoint.sh
@@ -4,14 +4,7 @@ python -c "import re;import os;out=re.sub('(#SecAction[\S\s]{7}id:900000[\s\S]*t
 python -c "import re;import os;out=re.sub('(#SecAction[\S\s]{6}id:900330[\s\S]*total_arg_length=64000\")','SecAction \\\\\n \"id:900330, \\\\\n phase:1, \\\\\n nolog, \\\\\n pass, \\\\\n t:none, \\\\\n setvar:tx.total_arg_length=64000\"',open('/etc/modsecurity.d/owasp-crs/crs-setup.conf','r').read());open('/etc/modsecurity.d/owasp-crs/crs-setup.conf','w').write(out)" && \
 
 if [ $WEBSERVER = "Apache" ]; then
-  if [ ! -z $PROXY ]; then
-    if [ $PROXY -eq 1 ]; then
-      WEBSERVER_ARGUMENTS='-D crs_proxy'
-      if [ -z "$UPSTREAM" ]; then
-        export UPSTREAM=$(/sbin/ip route | grep ^default | perl -pe 's/^.*?via ([\d.]+).*/$1/g'):81
-      fi
-    fi
-  fi
+  WEBSERVER_ARGUMENTS=''
 elif [ $WEBSERVER = "Nginx" ]; then
   WEBSERVER_ARGUMENTS=''
 fi
