f6ed90bf2eb73594efd67d233a738ae8fcfaf50d
Manuel Spartan <spartantri@gmail.com>
2019-09-17 16:25:06 -0400

    Fix bad sed and sync to v3.2

--- util/send-payload-pls.sh
+++ util/send-payload-pls.sh
@@ -1,17 +1,91 @@
 #!/bin/bash
 #
-# Script to post a payload against a local webserver at each paranoia level
+# Script to post a payload against a local webserver at each paranoia level.
 #
-# Note: Webserver has to be prepared to take desired PL as Request Header "PL"
+# Note: Webserver has to be prepared to take desired PL as Request Header "PL".
+
+# WARNING: Setting the paranoia level using a header without proper authentication and authorization
+# is extremely dangerous, and is not recommended for production.
+
 # Check the access log format at https://www.netnea.com/cms/apache-tutorial-5_extending-access-log/
+# This script assumes %{ModSecAnomalyScoreIn}e is the last access log column
+# For better results use the collaborative mode and set the SecDefaultAction to 'pass'
+# 
+# The anomaly score envvar can be set as follows:
+# SecAction "id:90101,phase:5,pass,nolog,setenv:ModSecAnomalyScoreIn=%{TX.anomaly_score}"
 #
+# Sample rule to setup the PL dinamically from localhost"
+#SecRule REMOTE_ADDR "@ipMatch 127.0.0.1,192.168.0.128" "id:90102,phase:1,pass,capture,log,auditlog,msg:'Setting engine to PL%{matched_var}',chain"
+#    SecRule REQUEST_HEADERS:PL "@rx ([1-4])" "setvar:'tx.executing_paranoia_level=%{matched_var}'"
+
 # Path to CRS rule set and local files
-CRS="/usr/share/modsecurity-crs/rules/"
+CRS="/usr/share/modsecurity-crs/rules"
 accesslog="/apache/logs/access.log"
 errorlog="/apache/logs/error.log"
+URL="localhost:40080"
+protocol="http"
+while [[ $# > 0 ]]
+do
+    case "$1" in
+        -c|--crs)
+            CRS="$2"
+            shift
+            ;;
+        -a|--access)
+            accesslog="$2"
+            shift
+            ;;
+        -e|--error)
+            errorlog="$2"
+            shift
+            ;;
+        -u|--url)
+            URL="$2"
+            shift
+            ;;
+        -r|--resolve)
+            resolve="$2"
+            resolve="--resolve $resolve"
+            shift
+            ;;
+        --protocol)
+            protocol="$2"
+            shift
+            ;;
+        --payload)
+            PAYLOAD="$2"
+            shift
+            ;;
+        -h|--help)
+            echo "Usage:"
+            echo " --access \"/apache/logs/access.log\""
+            echo " --error \"/apache/logs/error.log\""
+            echo " --url \"localhost:40080\""
+            echo " --resolve \"someservername:40080:localhost\""
+            echo " --protocol \"https\""
+            echo " --payload \"/tmp/payload\""
+            echo " --help"
+            exit 1
+            ;;
+    esac
+    shift
+done
+	    echo "Using CRS: $CRS"
+	    echo "Using accesslog: $accesslog"
+	    echo "Using errorlog: $errorlog"
+	    echo "Using URL: $URL"
+	    echo "Using protocol: $protocol"
+
+if [ -z "${PAYLOAD+x}" ]; then
+    echo "Please submit valid payload file as parameter. This is fatal. Aborting."
+    $0 -h
+    echo "Examples:"
+    echo "  ./send-payload-pls.sh -a /logs/test/access.log -e /logs/test/error.log -u test.test.test.com:6443 --protocol https --payload /tmp/payload --resolve test.test.test.com:6443:192.168.0.128"
+    echo "  ./send-payload-pls.sh -a /logs/test/access.log -e /logs/test/error.log -u test.test.test.com:6443 --protocol https --payload 'or 1=1;--' --resolve test.test.test.com:6443:192.168.0.128"
+    exit 1
+fi
 
 # URL of web server
-URL="localhost:40080"
 
 # Rules per Paranoia level
 # Paranoia level 1 rules, rule 012 is the phase 2 rule delimiter of the start of PL1
@@ -30,13 +104,6 @@ PL3=$(awk "/016,phase:2/,/017,phase:1/" $CRS/*.conf |egrep -v "(016|017),phase" 
 # Paranoia level 4 rules, "Paranoia Levels Finished" delimiter of the finish of PL4
 PL4=$(awk "/018,phase:2/,/Paranoia Levels Finished/" $CRS/*.conf |egrep -v "018,phase" |egrep -o "id:[0-9]+" |sed -r 's,id:([0-9]+),\1\\,' |tr -t '\n' '\|' |sed -r 's,\\\|$,,')
 
-if [ ! -z "$1" ]; then
-        PAYLOAD="$1"
-else
-        echo "Please submit payload as parameter. This is fatal. Aborting."
-        exit 1
-fi
-
 echo "Sending the following payload at multiple paranoia levels: $PAYLOAD"
 echo
 
@@ -44,13 +111,28 @@ for PL in 1 2 3 4; do
         echo "--- Paranoia Level $PL ---" 
         echo
         if [ -f "$PAYLOAD" ]; then
-                curl $URL --data-binary "@$PAYLOAD" -H "PL: $PL" -o /dev/null -s
+            curl $protocol://$URL $resolve -k --data-binary "@$PAYLOAD" -H "PL: $PL" -o /dev/null -s
         else
-                curl $URL -d "$PAYLOAD" -H "PL: $PL" -o /dev/null -s
-        fi 
-        grep $(tail -1 $accesslog | cut -d\" -f11 | cut -b2-26) $errorlog | sed -e "s/.*\[id \"//" -e "s/\(......\).*\[msg \"/\1 /" -e "s/\"\].*//" -e "s/(Total .*/(Total ...) .../" -e "s/Incoming and Outgoing Score: [0-9]* [0-9]*/Incoming and Outgoing Score: .../" | sed -e "s/$PL1/& PL1/" -e "s/$PL2/& PL2/" -e "s/$PL3/& PL3/ "-e "s/$PL4/& PL4/" | sort -k2
+            curl $protocol://$URL $resolve -k -d "$PAYLOAD" -H "PL: $PL" -o /dev/null -s
+        fi
+        # Here are three ways to get the transaction unique id, the first one is follini format, second is spartan format,
+        # and the third one tries to guess which is the unique id using a regular expression, the first two require specific format
+        # the third one may cause false positives, uncomment only the required format.
+	# To use Christian's accesslog format uncomment the following line
+        uniq_id=$(tail -1 $accesslog | cut -d\" -f11 | cut -b2-26)
+	# To use the 21st column (space delimited) uncomment the following line
+        #uniq_id=$(tail -1 $accesslog | awk '{print $21}')
+	# To use the automatic unique_id recognition uncomment the following line (may cause errors)
+        #uniq_id=$(tail -1 $accesslog | egrep -o '[a-zA-Z0-9]{26,28}')
+        echo "Tracking unique id: $uniq_id"
+
+        grep $uniq_id $errorlog | sed -e "s/.*\[id \"//" -e "s/\(......\).*\[msg \"/\1 /" -e "s/\"\].*//" -e "s/(Total .*/(Total ...) .../" -e "s/Incoming and Outgoing Score: [0-9]* [0-9]*/Incoming and Outgoing Score: .../" | sed -e "s/$PL1/& PL1/" -e "s/$PL2/& PL2/" -e "s/$PL3/& PL3/ " -e "s/$PL4/& PL4/" | sort -k2 | sed -r "s/^([0-9]+)$/\1 FOREIGN RULE NOT IN CRS/"
         echo
         echo -n "Total Incoming Score: "
-        tail -1 $accesslog | cut -d\" -f11 | cut -d\  -f14 | tr "-" "0" 
+        # Here are two ways to get the transaction anomaly score, the first one is follini format, second is spartan format
+	# To use Christian's accesslog format uncomment the following line
+        tail -1 $accesslog | cut -d\" -f11 | cut -d\  -f14 | tr "-" "0"
+	# To use the accesslog format with the anomaly score at the end of the line uncomment the following line
+        #tail -1 $accesslog | awk '{print $NF}' | tr "-" "0"
         echo
 done
